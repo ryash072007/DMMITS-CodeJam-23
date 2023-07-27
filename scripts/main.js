9df0d4643e3823e2959201c47b5394469c6cf50d
Evan Tobias <105860929+EchoRover@users.noreply.github.com>
2023-07-27 20:08:13 +0400

    updated

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,26 @@
 function scrollToBottom() {
     document.body.scrollIntoView(false);
 }
+function getpos(max,min){
+    return Math.random() * (max-min) + min;
+}
+function createStars(count){
+    const container = document.getElementById("stars")
+    
+    for (let i = 0;i < count; i++){
+
+        const star = document.createElement('div')
+        
+        star.classList.add('star')
+        star.style.top = `${getpos(0,200)}px`
+        star.style.right = `${getpos(0,1000)}px`
+        container.appendChild(star)
+
+    }
+    
+    
+}
+
+
+
+createStars(20)
