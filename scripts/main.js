fc3f2698c9aad82806a0d1e2822f75611eff4ce4
Evan Tobias <105860929+EchoRover@users.noreply.github.com>
2023-07-29 23:16:25 +0400

    reformat doc

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@ function scrollToBottom() {
     window.scroll(0, document.body.scrollHeight);
 }
 
-function addstar(){
+function addstar() {
     starsColleted += 1;
 }
 
@@ -14,54 +14,53 @@ function scrollleft() {
     const container = document.getElementById("slide")
 
     container.scrollLeft += 500;
-       
+
 }
 function scrollUp(val) {
     window.scrollBy(0, -val);
-  }
+}
 
-function transtion(){
+function transtion() {
     const container = document.getElementById("slide")
-    console.log(container.scrollLeft)
     const title = document.getElementById("title-end")
     const subtitle = document.getElementById("subtitle")
 
 
-    if (container.scrollLeft > 1500){
-        
-        if(title.innerHTML == "ISRO"){
+    if (container.scrollLeft > 1500) {
+
+        if (title.innerHTML == "ISRO") {
             return
         }
-        
+
         title.style.opacity = 0
         title.innerHTML = "ISRO"
         subtitle.innerHTML = "Indian Space Research Organisation"
 
-        setTimeout(function(){
+        setTimeout(function () {
             title.style.opacity = 1;
-        },500)
+        }, 500)
 
-    }else{
-        if(title.innerHTML == "INCOSPAR"){
+    } else {
+        if (title.innerHTML == "INCOSPAR") {
             return
         }
-        
-        
+
+
         title.style.opacity = 0
         title.innerHTML = "INCOSPAR"
         subtitle.innerHTML = "Indian National Committee for Space Research"
 
-        setTimeout(function(){
+        setTimeout(function () {
             title.style.opacity = 1;
-        },500)
+        }, 500)
     }
 
 }
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const scrollableDiv = document.getElementById("slide");
-    scrollableDiv.addEventListener("mouseenter", function() {
+    scrollableDiv.addEventListener("mouseenter", function () {
         scrollableDiv.addEventListener("scroll", transtion);
-      });
-
-     
     });
+
+
+});
