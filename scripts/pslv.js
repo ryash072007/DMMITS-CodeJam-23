982a8dcdc21ba6e0c5ff4d06b541c145f66c7ba2
Ryash <ryash072007@gmail.com>
2023-07-30 18:33:35 +0400

    idk2

---
+++ scripts/pslv.js
@@ -0,0 +1,24 @@
+function show1() {
+    document.getElementById("topic").innerHTML = "Payload Fairing and Fourth Stage";
+    document.getElementById('image_on_click').setAttribute('src', 'styles/images/PSLV - Copy.png');
+}
+
+function show2() {
+    document.getElementById("topic").innerHTML = "Third Stage";
+    document.getElementById('image_on_click').setAttribute('src', 'styles/images/PSLV - Copy (2).png');
+}
+
+function show3() {
+    document.getElementById("topic").innerHTML = "Second Stage";
+    document.getElementById('image_on_click').setAttribute('src', 'styles/images/PSLV - Copy (3).png');
+}
+
+function show4() {
+    document.getElementById("topic").innerHTML = "First Stage";
+    document.getElementById('image_on_click').setAttribute('src', 'styles/images/PSLV - Copy (4).png');
+}
+
+function show5() {
+    document.getElementById("topic").innerHTML = "Strap-on Boosters";
+    document.getElementById('image_on_click').setAttribute('src', 'styles/images/PSLV - Copy (5).png');
+}
